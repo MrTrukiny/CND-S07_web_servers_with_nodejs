5d8bd24a09b853d32abab6703856e910c562e05d
MrTrukiny <carloseduardo.tkn@gmail.com>
2023-05-26 23:06:50 -0500

    CND-S07: add routing, content-type headers and 404 not found

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  // Responding as text
-  // res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // res.end('Hello! Sir Isaac Newton is your friend!');
+const server = http.createServer();
 
-  // Responding as JSON
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    // JSON.stringify() converts a JavaScript object or value to a JSON string
-    // It is necessary to convert the object to a string before sending the response
-    JSON.stringify({
-      id: 1,
-      name: 'Sir Isaac Newton',
-    })
-  );
+server.on('request', (req, res) => {
+  if (req.url === '/friends') {
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // We can manually set the status code and headers
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: 'Sir Isaac Newton',
+      })
+    );
+  } else if (req.url === '/messages') {
+    // Status code defaults to 200
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
